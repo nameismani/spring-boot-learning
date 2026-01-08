1f82c119088a9c257c4f26114a13f80e0b99d281
nameismani <mm2729025@gmail.com>
2026-01-08 20:56:38 +0530

    Added dev tools and model thing

--- src/main/java/com/nameismani/spring_boot_learning/controller/UserController.java
+++ src/main/java/com/nameismani/spring_boot_learning/controller/UserController.java
@@ -1,16 +1,20 @@
 package com.nameismani.spring_boot_learning.controller;
 
+import java.util.*;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.nameismani.spring_boot_learning.model.User;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
 
     @GetMapping
-    public String getUsers(){
-        return "Check sdf";
+    public List<User> getUsers(){
+        return Arrays.asList(new User(1L,"Mani","nameismani@gmail.com"),new User(2L,"Sakthi","sakthi@gmail.com"));
     }    
 
 }
---
+++ src/main/java/com/nameismani/spring_boot_learning/model/User.java
@@ -0,0 +1,30 @@
+package com.nameismani.spring_boot_learning.model;
+
+public class User {
+private Long id;
+private String name;
+private String email;
+public User(Long id, String name, String email) {
+    this.id = id;
+    this.name = name;
+    this.email = email;
+}
+public void setId(Long id) {
+    this.id = id;
+}
+public void setName(String name) {
+    this.name = name;
+}
+public void setEmail(String email) {
+    this.email = email;
+}
+public Long getId() {
+    return id;
+}
+public String getName() {
+    return name;
+}
+public String getEmail() {
+    return email;
+}
+}
